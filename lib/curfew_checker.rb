6082187ce2c1ea881a6cedf71a3360a785912062
christinasherpa <chrisher24@charter.newvisions.org>
2018-08-07 18:55:40 +0000

    Done.

--- lib/curfew_checker.rb
+++ lib/curfew_checker.rb
@@ -16,4 +16,17 @@ end
 
 def platinum_curfew_checker(current_time, curfew_time)
   # code goes here
+ if current_time == curfew_time
+   puts "time to apparate"
+   elsif current_time > curfew_time
+   puts "you are late"
+   elsif current_time < curfew_time
+   time_left = curfew_time - current_time
+   puts "You have #{time_left} hours left!"
+ end
 end
+
+platinum_curfew_checker(11,11)
+platinum_curfew_checker(12,11)
+platinum_curfew_checker(9,11)
+
